54137a7ad96184b889becab2ab1313ae56c396ec
trentstehbenshl <trents@hlaustralia.com.au>
2022-06-20 16:02:43 +0930

    Add more comments

--- index-attempt.js
+++ index-attempt.js
@@ -11,7 +11,7 @@ const outputFile = 'index.html';
 
 // Create the initial html file
 function createInitialHtml() {
-    var initialHtml = '<html>\n<head>\n<title>SSL Certificate Expiry Dates</title>\n</head>\n<body>\n';
+    let initialHtml = '<html>\n<head>\n<title>SSL Certificate Expiry Dates</title>\n</head>\n<body>\n';
     fs.writeFile(outputFile, initialHtml, function(err) {
         if (err) {
             console.log(err);
@@ -22,7 +22,7 @@ function createInitialHtml() {
 
 // Append the expiry date to the html file
 function appendHtml(url, expiryDate) {
-    var expiryHtml = '\n <p>The SSL certificate for ' + url + ' expires on ' + expiryDate + '</p>';
+    let expiryHtml = '\n <p>The SSL certificate for ' + url + ' expires on ' + expiryDate + '</p>';
     console.log('appendHtml: ' + expiryHtml + url + expiryDate);
     fs.appendFile(outputFile, expiryHtml, function(err) {
         if (err) {
@@ -34,31 +34,31 @@ function appendHtml(url, expiryDate) {
 
 // Loop through the list of URLs in the file and build the html file
 function iterateOverURLs() {
-    var urlList = fs.readFileSync(urlFile).toString().split('\n');
+    let urlList = fs.readFileSync(urlFile).toString().split('\n');
     for (var i = 0; i < urlList.length; i++) {
-        var url = urlList[i];
+        let url = urlList[i];
         // Get expiry date of the SSL certificate
         console.log('Url is currently: ' + url);
-        var expiryDate = checkExpiry(url);
-        console.log('Expiry date is: ' + expiryDate);
-        appendHtml(url, expiryDate);
+        let expiryDateResult = checkExpiry(url);
+        console.log('Expiry date is: ' + expiryDateResult);
+        appendHtml(url, expiryDateResult);
     };
 };
 
 
 // Check expiry date of the given URL. Return the expiry date as a string.
 function checkExpiry(url) {
-    var expiryDate = '';
-    https.get(url, function(res) {
-        var cert = res.connection.getPeerCertificate();
-        expiryDate = cert.valid_to;
+    let expiryDate = '';
+    let pleaseBeExpiryDate = https.get(url, function(res) {
+        let cert = res.connection.getPeerCertificate();
+        let expiryDate = cert.valid_to;
         console.log('https get url function ' + url + ': ' + expiryDate); // Looks good
-        // Make expiryDate available to the main function?
+        // Debug: Make expiryDate available to the outside function? This isn't working
         return expiryDate;
     }).on('error', function(e) {
         console.log('https get url error: ' + e.message);
     }).end();
-    console.log('Outside expiry= ' + expiryDate);
+    console.log('Outside expiry= ' + expiryDate); // expirydate variable is still empty here
 
     // TODO: Need to wait for the https.get to finish before returning the expiry date
     // How do we get the expiryDate variable from within the above https.get function?
@@ -68,7 +68,7 @@ function checkExpiry(url) {
 
 // Complete the html file
 function completeHtml() {
-    var finalHtml = '</body></html>';
+    let finalHtml = '</body></html>';
     fs.appendFile(outputFile, finalHtml, function(err) {
         if (err) {
             console.log(err);
